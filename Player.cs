f2a1f64916ae57ca361d4514ae4df8886f3fefa4
Rafał Kryczyk <rafal.kryczyk@outlook.com>
2022-07-08 21:57:46 +0200

    Test

---
+++ Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    internal class Player
+    {
+        string name;
+        int currentScore = 0;
+        long highscore = 0;
+        int lives = 3;
+        int hotstreak = 0;
+        int highestRoundHotstreak = 0;
+        int highestHotstreak = 0;
+
+        public Player(string name)
+        {
+            this.name = name;           
+        }
+    }
+}
